9019b31d8ad23dbd2ff3fc32b26a1fc7db63c9f1
Thomas Mailund <mailund@birc.au.dk>
2021-07-01 07:17:07 +0200

    removed duplicated code

--- test_programs/choices/choices.go
+++ test_programs/choices/choices.go
@@ -3,55 +3,19 @@ package main
 import (
 	"fmt"
 	"os"
-	"strings"
 
 	"github.com/mailund/cli"
-	"github.com/mailund/cli/interfaces"
 )
 
-type Choice struct {
-	Choice  string
-	Options []string
-}
-
-func (c *Choice) Set(x string) error {
-	for _, v := range c.Options {
-		if v == x {
-			c.Choice = v
-			return nil
-		}
-	}
-
-	return interfaces.ParseErrorf(
-		"%s is not a valid choice, must be in %s", x, "{"+strings.Join(c.Options, ",")+"}")
-}
-
-func (c *Choice) String() string {
-	return c.Choice
-}
-
-func (c *Choice) ArgumentDescription(flag bool, descr string) string {
-	if flag {
-		// Don't modify the flag description (we handle it on the value)
-		return descr
-	}
-
-	return descr + " (choose from " + "{" + strings.Join(c.Options, ",") + "})"
-}
-
-func (c *Choice) FlagValueDescription() string {
-	return "{" + strings.Join(c.Options, ",") + "}"
-}
-
 type Args struct {
-	A Choice `flag:"" short:"a" descr:"optional choice"`
-	B Choice `pos:"b" descr:"mandatory choice"`
+	A cli.Choice `flag:"" short:"a" descr:"optional choice"`
+	B cli.Choice `pos:"b" descr:"mandatory choice"`
 }
 
 func Init() interface{} {
 	return &Args{
-		A: Choice{"A", []string{"A", "B", "C"}},
-		B: Choice{"B", []string{"A", "B", "C"}},
+		A: cli.Choice{Choice: "A", Options: []string{"A", "B", "C"}},
+		B: cli.Choice{Choice: "B", Options: []string{"A", "B", "C"}},
 	}
 }
 
