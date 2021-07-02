2a694f58ebd919229b0bd2b684a2c88f4fe5486b
Thomas Mailund <mailund@birc.au.dk>
2021-07-02 09:05:07 +0200

    simplified prepare as well

--- prepare.go
+++ prepare.go
@@ -4,43 +4,50 @@ import (
 	"github.com/mailund/cli/interfaces"
 )
 
+func prepare(i interface{}) error {
+	if v, ok := i.(interfaces.Prepare); ok {
+		return v.PrepareValue()
+	}
+
+	return nil
+}
+
 func prepareFlagsAndParams(cmd *Command) error {
+	var err error
+
 	for i := 0; i < cmd.flags.NFlags(); i++ {
-		if v, ok := cmd.flags.Flag(i).Value.(interfaces.Prepare); ok {
-			if err := v.PrepareValue(); err != nil {
-				f := cmd.flags.Flag(i)
-				flagname := ""
-
-				if f.Short != "" {
-					flagname += "-" + f.Short
-				}
-
-				if f.Long != "" {
-					if f.Short != "" {
-						flagname += ","
-					}
-
-					flagname += "--" + f.Long
-				}
-
-				return interfaces.ParseErrorf("error in flag %s: %s", flagname, err)
-			}
+		if err = prepare(cmd.flags.Flag(i).Value); err == nil {
+			continue
 		}
+
+		// we have an error, but need a better error message
+		f := cmd.flags.Flag(i)
+		flagname := ""
+
+		if f.Short != "" {
+			flagname += "-" + f.Short
+		}
+
+		if f.Long != "" {
+			if f.Short != "" {
+				flagname += ","
+			}
+
+			flagname += "--" + f.Long
+		}
+
+		return interfaces.ParseErrorf("error in flag %s: %s", flagname, err)
 	}
 
 	for i := 0; i < cmd.params.NParams(); i++ {
-		if v, ok := cmd.params.Param(i).Value.(interfaces.Prepare); ok {
-			if err := v.PrepareValue(); err != nil {
-				return interfaces.ParseErrorf("error in argument %s: %s", cmd.params.Param(i).Name, err)
-			}
+		if err = prepare(cmd.params.Param(i).Value); err != nil {
+			return interfaces.ParseErrorf("error in argument %s: %s", cmd.params.Param(i).Name, err)
 		}
 	}
 
 	if vv := cmd.params.Variadic(); vv != nil {
-		if v, ok := vv.Value.(interfaces.Prepare); ok {
-			if err := v.PrepareValue(); err != nil {
-				return interfaces.ParseErrorf("error in argument %s: %s", cmd.params.Variadic().Name, err)
-			}
+		if err := prepare(vv.Value); err != nil {
+			return interfaces.ParseErrorf("error in argument %s: %s", cmd.params.Variadic().Name, err)
 		}
 	}
 
