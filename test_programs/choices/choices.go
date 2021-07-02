4f525ec8b3e2da4100b5086bfe63e0e896cfae8a
Thomas Mailund <mailund@birc.au.dk>
2021-07-02 08:12:19 +0200

    don't export from choices test program

--- test_programs/choices/choices.go
+++ test_programs/choices/choices.go
@@ -7,20 +7,20 @@ import (
 	"github.com/mailund/cli"
 )
 
-type Args struct {
+type args struct {
 	A cli.Choice `flag:"" short:"a" descr:"optional choice"`
 	B cli.Choice `pos:"b" descr:"mandatory choice"`
 }
 
-func Init() interface{} {
-	return &Args{
+func initChoices() interface{} {
+	return &args{
 		A: cli.Choice{Choice: "A", Options: []string{"A", "B", "C"}},
 		B: cli.Choice{Choice: "B", Options: []string{"A", "B", "C"}},
 	}
 }
 
-func Action(i interface{}) {
-	args, _ := i.(*Args)
+func choicesAction(i interface{}) {
+	args, _ := i.(*args)
 	fmt.Printf("Choice A was %s and choice B was %s\n", args.A.Choice, args.B.Choice)
 }
 
@@ -29,8 +29,8 @@ func main() {
 		cli.CommandSpec{
 			Name:   "choices",
 			Long:   "Demonstration of the difficult task of making choices.",
-			Init:   Init,
-			Action: Action,
+			Init:   initChoices,
+			Action: choicesAction,
 		})
 
 	cmd.Run(os.Args[1:])
